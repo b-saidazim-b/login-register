0ae2dbbd0ca1e40a90eb646922a177c70f3a39d6
b-saidazim-b <saidazimbakhodirof@gmail.com>
2021-10-10 16:26:14 +0500

    2.tanla: reg or log

--- aa.py
+++ aa.py
@@ -1,9 +1,31 @@
+import os
 class User:
     def __init__(self, login, password, age, gender):
         self.login = login
         self.password = password
         self.age = age
         self.gender = gender
+        self.tanla_values = ['0', '1', '2']
+
+    def tanla(self):
+        print('''
+        [0] ---> Exit
+        [1] ---> Register
+        [2] ---> Log In
+        ''')
+
+    def tanla_option(self):
+        self.tanla()
+        inp_tanla = input('[1, 2, 3]: ')
+        while inp_tanla not in self.tanla_values:
+            self.clear()
+            self.tanla()
+            inp_tanla = input('[1, 2, 3]: ')
+
+    @staticmethod
+    def clear():
+        os.system('clear')
 
 
-        
+man = User('qwe', 'qwe', 'qwe', 'qwe')
+man.tanla_option()
