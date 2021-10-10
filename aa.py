f23ea413dc4f0592afd9e9005ce37c8619297b4c
b-saidazim-b <saidazimbakhodirof@gmail.com>
2021-10-10 16:11:27 +0500

    1.Class ochtim

---
+++ aa.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, login, password, age, gender):
+        self.login = login
+        self.password = password
+        self.age = age
+        self.gender = gender
+
+
+        
